0a1491224b1950e33cdb2950e52a73053cb7bdf2
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-03 18:06:48 +0200

    Math covered

---
+++ 05-Math.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Math</title>
+</head>
+
+<body>
+    <form action="05-Math.php" method="POST">
+        <label for="x">x:</label>
+        <input type="text" name="x">
+        <br>
+        <label for="y">y:</label>
+        <input type="text" name="y">
+        <br>
+        <label for="z">z:</label>
+        <input type="text" name="z">
+        <input type="submit" value="total">
+    </form>
+</body>
+
+</html>
+
+<?php
+$x = $_POST["x"];
+$y = $_POST["y"];
+$z = $_POST["z"];
+$total = null;
+
+// $total = abs($x);
+// $total = round($x);
+// $total = floor($x);
+// $total = ceil($x);
+
+// $total = pow($x, $y);
+// $total = sqrt($x);
+
+// $total = max($x, $y, $z);
+// $total = min($x, $y, $z);
+// $total = pi();
+// $total = rand(1, 20);
+echo $total;
+?>
