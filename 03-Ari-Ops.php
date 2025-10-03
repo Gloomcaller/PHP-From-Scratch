85d72e8686e40d92e5529369509952fdbe44eab2
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-03 17:18:14 +0200

    Operators covered.

---
+++ 03-Ari-Ops.php
@@ -0,0 +1,19 @@
+<?php
+$x = 10;
+$y = 2;
+$z = null;
+
+// $z = $x + $y; plus
+// $z = $x - $y; minus
+// $z = $x * $y; times
+// $z = $x / $y; divided
+// $z = $x ** $y; power
+// $z = $x % $y; remainder
+
+$counter = 0;
+
+// $counter++; +1
+// $counter--; -1
+// $counter+= n; +n
+// $counter-= n; -n
+?>
