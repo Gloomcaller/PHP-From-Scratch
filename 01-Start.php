6d63dc10997c3541a9032de6948778a16ccebfcb
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-03 16:51:35 +0200

    Starter started.

---
+++ 01-Start.php
@@ -0,0 +1,21 @@
+<?php
+echo "I like Sushi <br>";
+echo "It's nice."
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Starter</title>
+</head>
+
+<body>
+    <br>
+    <button>Sushi Button!</button>
+    <p>It don't work...</p>
+</body>
+
+</html>
