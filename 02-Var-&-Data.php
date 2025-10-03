16813c93f57ac11962f0e639afb1fcf3d288916f
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-03 17:08:34 +0200

    Vars varied.

---
+++ 02-Var-&-Data.php
@@ -0,0 +1,22 @@
+<?php
+$name = "Bobo Smrade";
+$food = "Lembas";
+$home = "Shire";
+
+$age = 34;
+$party = 9;
+
+$kills = 2.5;
+$gold = 43.2;
+
+$ring = true;
+
+echo "Hello {$name}<br>";
+echo "You like {$food}<br>";
+echo "You live in the {$home}<br>";
+echo "You are {$age} years old<br>";
+echo "There are {$party} members in your party<br>";
+echo "You have killed {$kills} orcs so far<br>";
+echo "There is \${$gold} gold in your pouch<br>";
+echo "Do you have the ring: {$ring}<br>"
+    ?>
