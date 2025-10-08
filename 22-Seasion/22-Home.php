51276b0be2bd10424639ea9bc57988b4ddff5123
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-08 16:28:56 +0200

    Sessions

---
+++ 22-Seasion/22-Home.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    This is the home page.<br>
+    <form action="22-Home.php" method="POST">
+        <input type="submit" name="logout" value="logout">
+    </form>
+</body>
+
+</html>
+
+<?php
+echo $_SESSION["username"] . "<br>";
+echo $_SESSION["password"] . "<br>";
+
+if (isset($_POST["logout"])) {
+    session_destroy();
+    header("Location: 22-Session.php");
+}
+?>
