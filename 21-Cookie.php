ddaa85c0a0fd8640e1796c5c338807488558edf6
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-08 14:32:52 +0200

    Monster

---
+++ 21-Cookie.php
@@ -0,0 +1,11 @@
+<?php
+setcookie("fav_food", "pizza", time() + (86400 * 2), "/");
+setcookie("fav_drink", "coffee", time() + (86400 * 3), "/");
+setcookie("fav_dessert", "ice cream", time() + (86400 * 4), "/");
+
+if (isset($_COOKIE["fav_food"])) {
+    echo "BUY SOME {$_COOKIE["fav_food"]} !!!";
+} else {
+    echo "I don't know your favorite food";
+}
+?>
