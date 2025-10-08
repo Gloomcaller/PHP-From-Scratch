ab120a85cb95822b0b98da85f435a5d309f17e7a
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-08 14:25:17 +0200

    Include

---
+++ 20-Include/20-Include.php
@@ -0,0 +1,23 @@
+<?php
+include("20-Header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Include</title>
+</head>
+
+<body>
+    This is the Home page<br>
+    Stuff about your Home page can go here<br>
+</body>
+
+</html>
+
+<?php
+include("20-Footer.html");
+?>
---
+++ 20-Include/20-Locations.php
@@ -0,0 +1,23 @@
+<?php
+include("20-Header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Locations</title>
+</head>
+
+<body>
+    This is the Locations page<br>
+    Stuff about your Locations page can go here<br>
+</body>
+
+</html>
+
+<?php
+include("20-Footer.html");
+?>
