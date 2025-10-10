5a0da861d352acf18cac35b626b884dafa325257
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-10 17:24:42 +0200

    Reg Form mini

---
+++ 30-Reg-Form.php
@@ -0,0 +1,51 @@
+<?php
+include("27-Database-Connection.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+        <h2>Wellcome to the Registration Form</h2>
+        Username:<br>
+        <input type="text" name="username"><br>
+        Password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="register"><br>
+    </form>
+</body>
+
+</html>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (empty($username)) {
+        echo "Please enter a username.";
+    } else if (empty($password)) {
+        echo "Please enter a password.";
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash')";
+
+        try {
+            mysqli_query($conn, $sql);
+            header("Location: " . $_SERVER['PHP_SELF']);
+            exit();
+        } catch (mysqli_sql_exception) {
+            echo "Erro occured.";
+        }
+    }
+}
+
+mysqli_close($conn);
+?>
