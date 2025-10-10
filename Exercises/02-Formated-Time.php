81e38f5180c118e76298df0ee201880221520b61
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-10-10 20:54:37 +0200

    Forgot one.

--- Exercises/02-Formated-Time.php
+++ Exercises/02-Formated-Time.php
@@ -1,4 +1,9 @@
 <?php
-$curr_td = date("d.m.Y H:i:s");
-echo "Current date & time: $curr_td";
+$curr_td_one = date("d.m.Y H:i:s");
+echo "Current date & time (first type): $curr_td_one";
+
+echo "<br><br>";
+
+$curr_td_two = date("l, d. F Y, h:i A");
+echo "Current date & time (second type): $curr_td_two";
 ?>
